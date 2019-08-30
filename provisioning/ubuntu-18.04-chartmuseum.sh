a32cd6f373ba61ab3952d47176876b894b70c60c
Urs Voegele <unext@bluewin.ch>
2019-08-30 11:36:44 +0200

    provisioning for owner and group charts directory changed

--- provisioning/ubuntu-18.04-chartmuseum.sh
+++ provisioning/ubuntu-18.04-chartmuseum.sh
@@ -5,13 +5,18 @@ sudo apt update
 
 # install chartmuseum
 IMAGETAG="0.9.0"
+LOCALDIR="/home/vagrant/charts"
 echo "install chartmuseum version $IMAGETAG"
 sudo docker run -d --restart=unless-stopped --name chartmuseum -p 8080:8080 \
-     -v /home/vagrant/charts:/charts \
+     -v $LOCALDIR:/charts \
      -e DEBUG=true \
      -e STORAGE=local \
      -e STORAGE_LOCAL_ROOTDIR=/charts \
      chartmuseum/chartmuseum:v$IMAGETAG
+
+# change owner & group
+sudo chown -R vagrant $LOCALDIR
+sudo chgrp -R vagrant $LOCALDIR
 
 # create date string
 DATE=`date +%Y%m%d%H%M`
