be74139e067e51c5b3046c0f281e972b1a514527
Urs Voegele <unext@bluewin.ch>
2021-03-08 15:04:17 +0100

    test Vagrantfile changed

--- test/download/cleanup-vagrant.ps1
+++ test/download/cleanup-vagrant.ps1
@@ -4,4 +4,11 @@ if (Test-Path $VagrantDirName) {
   Remove-Item $VagrantDirName -Recurse
 }
 
+# delete log file
+$LogFilename = 'ubuntu-xenial-16.04-cloudimg-console.log'
+if (Test-Path $LogFilename) {
+  Remove-Item $LogFilename
+}
+
+
 
